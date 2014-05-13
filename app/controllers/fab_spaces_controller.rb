4775f6a84ba72cfaa7af47c40d80bdba97aca696
John Rees <john@bitsushi.com>
2014-05-13 19:11:02 +0200

    fab space friendly

--- app/controllers/fab_spaces_controller.rb
+++ app/controllers/fab_spaces_controller.rb
@@ -1,6 +1,7 @@
 class FabSpacesController < InheritedResources::Base
   load_and_authorize_resource
   skip_authorize_resource :only => :map
+  defaults resource_class: FabSpace.friendly
 
   def map
     @fab_spaces = FabSpace.all
