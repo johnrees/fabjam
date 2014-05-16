91da52e85a9522cf97640aa4d7d3823333a86c6d
John Rees <john@bitsushi.com>
2014-05-16 12:10:16 +0200

    news formatting

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,8 @@
 class StaticController < ApplicationController
   def home
     @fab_spaces = FabSpace.order("id DESC").limit(3)
-    @posts = Post.where("published_at > ?", Time.now).order('published_at DESC').limit(3)
+    @posts = Post.order('published_at DESC').limit(3)
+    # .where("published_at > ?", Time.now)
   end
 
   def about
