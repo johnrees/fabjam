4f602f097313982fa4295172a914139b66f42b19
John Rees <john@bitsushi.com>
2014-05-17 17:16:31 +0200

    users have names

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
     me_path
   end
 
+before_filter :configure_devise_params, if: :devise_controller?
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_presence_of :first_name, :last_name, :email
+
   def to_s
     email
   end
