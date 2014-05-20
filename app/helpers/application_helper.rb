74e8a9f65d9f2b26be7c7d22b637791e894f9888
John Rees <john@bitsushi.com>
2014-05-20 16:41:37 +0200

    sticky footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
 
+
+  def title(page_title, options={})
+    content_for(:title, page_title.to_s)
+    return content_tag(:h1, page_title, options.merge(itemprop: "name"))
+  end
+
   def markdown(text)
     renderer = HTML.new(hard_wrap: true, filter_html: true)
     options = {
@@ -11,9 +17,6 @@ module ApplicationHelper
       superscript: true
     }
     Redcarpet::Markdown.new(renderer, options).render(text).html_safe
-  end
-
-  def title
   end
 
 end
